9871c800ae10e2a2d23cf345355aa7dfb6ed9110
John <johnclapper89@gmail.com>
2024-06-30 19:05:38 -0400

    Added Book Controller with routes to get all books and get book by id

---
+++ api/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+        public BookController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var books = _context.Books.ToList();
+
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var book = _context.Books.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+    }
+}
