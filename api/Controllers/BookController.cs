d72cd7980d6a728c4ca3757412c6a1880c7e6e5c
John <johnclapper89@gmail.com>
2024-06-30 19:16:19 -0400

    Fixed typo

--- api/Controllers/BookController.cs
+++ api/Controllers/BookController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    [Route("api/stock")]
+    [Route("api/book")]
     [ApiController]
     public class BookController : ControllerBase
     {
