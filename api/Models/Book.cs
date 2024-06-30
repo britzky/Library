5d6cfe1d2227488f5a184c58a8ae47a23e44824e
John <johnclapper89@gmail.com>
2024-06-30 18:04:59 -0400

    added Id for primary key

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -7,6 +7,7 @@ namespace api.Models
 {
     public class Book
     {
+        public int Id { get; set; }
         public int ISBN { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
