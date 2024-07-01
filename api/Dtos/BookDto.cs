3b672d300e86a38e32f815505d11e3729ce92dca
John <johnclapper89@gmail.com>
2024-07-01 18:44:31 -0400

    Added BookDto

---
+++ api/Dtos/BookDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public long ISBN { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string CoverImage { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public DateTime PublicationDate { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public int PageCount { get; set; }
+        public bool Availability { get; set; }
+    }
+}
