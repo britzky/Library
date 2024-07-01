3a5489203cedfb29bcc449bff624d15a71afcc12
John <johnclapper89@gmail.com>
2024-07-01 14:45:30 -0400

    Added availability to book model

---
+++ api/Migrations/20240701184452_AddAvailabilityToBooks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAvailabilityToBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Availability",
+                table: "Books",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Availability",
+                table: "Books");
+        }
+    }
+}
