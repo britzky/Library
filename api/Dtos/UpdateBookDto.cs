b464d4ba6611af3d4e87cd208c0357def5d9c31f
John <johnclapper89@gmail.com>
2024-07-02 15:40:46 -0400

    Added update book dto

---
+++ api/Dtos/UpdateBookDto.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos
+{
+    public class UpdateBookDto
+    {
+        public long ISBN { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string CoverImage { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public DateTime PublicationDate { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public int PageCount { get; set; }
+        public bool Availability { get; set; }
+    }
+}
