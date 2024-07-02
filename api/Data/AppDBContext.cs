952fc3b415eec8d71e3c32b1685448a10914838b
John <johnclapper89@gmail.com>
2024-07-02 19:39:22 -0400

    Added Identity and Roles

--- api/Data/AppDBContext.cs
+++ api/Data/AppDBContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class AppDBContext : DbContext
+    public class AppDBContext : IdentityDbContext
     {
         public AppDBContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)
@@ -16,5 +18,25 @@ namespace api.Data
         }
 
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Librarian",
+                    NormalizedName = "LIBRARIAN"
+                },
+                new IdentityRole
+                {
+                    Name = "Customer",
+                    NormalizedName = "CUSTOMER"
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
