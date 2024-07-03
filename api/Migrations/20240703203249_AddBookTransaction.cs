3359bc4425ec9e787a408eab619cd73d1eb924bc
John <johnclapper89@gmail.com>
2024-07-03 17:46:38 -0400

    Added Book Transaction model

---
+++ api/Migrations/20240703203249_AddBookTransaction.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a726e3cb-3370-46fd-af8f-2bcfd65b726d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c24496b8-502e-4fef-9b10-306e60bb221e");
+
+            migrationBuilder.CreateTable(
+                name: "BookTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CheckoutDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsReturned = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookTransactions_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookTransactions_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1240157c-5271-4653-a2f2-672bb52ea328", null, "Librarian", "LIBRARIAN" },
+                    { "631e366b-7a2e-440b-b55c-8278378de197", null, "Customer", "CUSTOMER" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTransactions_BookId",
+                table: "BookTransactions",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTransactions_UserId",
+                table: "BookTransactions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookTransactions");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1240157c-5271-4653-a2f2-672bb52ea328");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "631e366b-7a2e-440b-b55c-8278378de197");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "a726e3cb-3370-46fd-af8f-2bcfd65b726d", null, "Librarian", "LIBRARIAN" },
+                    { "c24496b8-502e-4fef-9b10-306e60bb221e", null, "Customer", "CUSTOMER" }
+                });
+        }
+    }
+}
