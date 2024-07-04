ef45ca433932f997d33a9bf4bfc066abcb482be2
John <johnclapper89@gmail.com>
2024-07-04 19:00:02 -0400

    Added Id and Availability

--- api/Dtos/FeaturedBookDto.cs
+++ api/Dtos/FeaturedBookDto.cs
@@ -7,9 +7,11 @@ namespace api.Dtos
 {
     public class FeaturedBookDto
     {
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string CoverImage { get; set; } = string.Empty;
+        public bool Availability { get; set; }
     }
 }
