e97d86971d6ec5f4da49e2fad7fd5d3878ea0c87
John <johnclapper89@gmail.com>
2024-07-05 12:23:39 -0400

    conditionally render buttons based on role and authentication state

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image"
+import { useAuth } from "../contexts/AuthContext";
 
 export const Navbar = () => {
-
+    const { user, logout } = useAuth();
   return (
     <div className="w-full">
         <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
@@ -25,9 +26,35 @@ export const Navbar = () => {
             </div>
 
             <div className="hidden mr-3 space-x-4 lg:flex nav__item">
+                {user ? (
+                    <>
+                        {user.role === "Librarian" ? (
+                            <Link
+                            href="/returned-books"
+                            className="px-6 py-2 text-white bg-blue-600 rounded-md md:ml-5"
+                            >
+                            Returned Books
+                            </Link>
+                        ) : (
+                            <Link
+                                href="/return-books"
+                                className="px-6 py-2 text-white bg-green-600 rounded-md md:ml-5"
+                            >
+                                Return Books
+                            </Link>
+                        )}
+                        <button
+                            onClick={logout}
+                            className="px-6 py-2 text-white bg-red-600 rounded-md md:ml-5"
+                        >
+                            Logout
+                        </button>
+                    </>
+                ) : (
                 <Link href="/auth" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
                     Login / Sign up
                 </Link>
+                )}
 
             </div>
         </nav>
